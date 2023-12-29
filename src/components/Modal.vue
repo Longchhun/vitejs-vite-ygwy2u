49642eb8205c1f449826bc1b4c28ce7c5da64005
Longchhun <50982518+Longchhun@users.noreply.github.com>
2023-12-29 15:13:26 +0700

    Initial commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+type PropsModal =
+  | {
+      variant: 'no-title';
+    }
+  | {
+      variant: 'title';
+      title: string;
+    };
+const props = defineProps<PropsModal>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div v-if="props."
+</template>
